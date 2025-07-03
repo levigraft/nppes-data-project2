1aff1e7c70d38cedce090e1078866b5e8fa59637
Alex Berka <alexanderberka@gmail.com>
2025-07-03 12:28:41 -0500

    Move batching process to python script

--- function_app.py
+++ function_app.py
@@ -70,8 +70,8 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     CLEAN_DATA_PROC = 'clean_data'
     COMPILE_DATA_PROC = 'compile_data'
     STORE_DATA_PROC = 'store_data'
-    BATCH_SIZE = 10000
-    COMMIT_SIZE = 1000027
+    BATCH_SIZE = 300000
+    COMMIT_SIZE = df.shape[0]
 
     with psycopg2.connect(
         dbname="project-2",
@@ -81,23 +81,24 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         port="5432"
     ) as conn:
         with conn.cursor() as cursor:
-            df_as_csv = df[:COMMIT_SIZE].write_csv()
-            ROW_COUNT = df[:COMMIT_SIZE].shape[0]
-            cursor.execute(f'TRUNCATE TABLE {RAW_DATA_TABLE};')
-
-            cursor.copy_expert(
-                f"COPY {RAW_DATA_TABLE} FROM STDIN WITH (FORMAT CSV, HEADER TRUE)",
-                io.StringIO(df_as_csv)
-            )
-
-            conn.commit()
-
             offset = 0
             batch_counter = []
-            while offset < ROW_COUNT:
+
+            while offset < COMMIT_SIZE:
                 batch_start = datetime.now()
 
-                cursor.execute(f'CALL {CLEAN_DATA_PROC}({BATCH_SIZE}, {offset});')
+                end_index = min(offset + BATCH_SIZE, COMMIT_SIZE)
+
+                df_as_csv = df[offset:end_index].write_csv()
+                
+                cursor.execute(f'TRUNCATE TABLE {RAW_DATA_TABLE};')
+
+                cursor.copy_expert(
+                    f"COPY {RAW_DATA_TABLE} FROM STDIN WITH (FORMAT CSV, HEADER TRUE)",
+                    io.StringIO(df_as_csv)
+                )
+
+                cursor.execute(f'CALL {CLEAN_DATA_PROC}();')
                 cursor.execute(f'CALL {COMPILE_DATA_PROC}();')
                 cursor.execute(f'CALL {STORE_DATA_PROC}();')
 
@@ -115,6 +116,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     print(f"START: {start_time}; END: {end_time};  ELAPSED: {elapsed} ms")
    
     return func.HttpResponse(
-            f"Connection successful. Elapsed time: {elapsed} ms. Batch times: {batch_counter}. Average batch time: {sum(batch_counter) / len(batch_counter)} ms.",
+            f"Connection successful / Elapsed time: {elapsed} ms / Batch times: {batch_counter} / Average batch time: {round(sum(batch_counter) / len(batch_counter))} ms / Total batch time: {round(sum(batch_counter))} ms",
             status_code=200
     )
